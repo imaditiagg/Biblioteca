467e64198931d20f0f906fd821c9ec71d5722a42
Aditi Aggarwal <aditi.aggarwal@thoughtworks.com>
2020-02-07 23:58:00 +0530

    [Aditi] Add. Invalid option in menu

--- src/main/java/com/twu/biblioteca/Biblioteca.java
+++ src/main/java/com/twu/biblioteca/Biblioteca.java
@@ -18,18 +18,24 @@ class Biblioteca {
     }
 
     private void printMenu() {
-        System.out.println("       ---------------- MENU --------------        ");
-        System.out.println("       1. View Books                               ");
+        System.out.println("         ---------------- MENU --------------        ");
+        System.out.println("         1. View Books                               ");
     }
 
     public static void main(String[] args) {
         Biblioteca biblioteca = new Biblioteca();
         biblioteca.printWelcomeMessage();
         biblioteca.printMenu();
+        System.out.println("\n");
+        System.out.println("         Enter your choice :  ");
         Scanner scanner = new Scanner(System.in);
         int choice = scanner.nextInt();
-        if (choice == 1) {
-            biblioteca.printListOfBooks();
+        switch (choice) {
+            case 1:
+                biblioteca.printListOfBooks();
+                break;
+            default:
+                System.out.println(Message.INVALID_OPTION);
         }
     }
 }
--- src/main/java/com/twu/biblioteca/Message.java
+++ src/main/java/com/twu/biblioteca/Message.java
@@ -2,4 +2,5 @@ package com.twu.biblioteca;
 
 class Message {
     static final String WELCOME = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
+    static final String INVALID_OPTION = "Please select a valid option";
 }
