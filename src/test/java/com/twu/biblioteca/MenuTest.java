fa97a40c026f633f4e422fcb1e0731adc781c645
Aditi Aggarwal <aditi.aggarwal@thoughtworks.com>
2020-02-10 19:04:06 +0530

    [Aditi] Refactor. Remove redundant test cases

--- src/test/java/com/twu/biblioteca/MenuTest.java
+++ src/test/java/com/twu/biblioteca/MenuTest.java
@@ -9,30 +9,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class MenuTest {
 
     @Test
-    void shouldAddViewBooksMenuItemToMenu() {
-        MenuItem menuItemOne = new ViewBooks();
-        ArrayList<MenuItem> menuItems = new ArrayList<>();
-        menuItems.add(menuItemOne);
-
-        Menu menu = new Menu(menuItems);
-
-        assertEquals(1, menu.noOfMenuItems());
-    }
-
-    @Test
-    void shouldAddTwoMenuItemsToMenu() {
-        MenuItem menuItemOne = new ViewBooks();
-        MenuItem menuItemTwo = new CheckoutBook();
-        ArrayList<MenuItem> menuItems = new ArrayList<>();
-        menuItems.add(menuItemOne);
-        menuItems.add(menuItemTwo);
-
-        Menu menu = new Menu(menuItems);
-
-        assertEquals(2, menu.noOfMenuItems());
-    }
-
-    @Test
     void shouldAddAllMenuItemsToMenu() {
         MenuItem menuItemOne = new ViewBooks();
         MenuItem menuItemTwo = new CheckoutBook();
