91e7813ebf76835db2766058f1767aab5fc93b35
Aditi Aggarwal <aditi.aggarwal@thoughtworks.com>
2020-02-10 19:03:18 +0530

    [Aditi] Add. MenuItems to Menu

--- src/test/java/com/twu/biblioteca/MenuTest.java
+++ src/test/java/com/twu/biblioteca/MenuTest.java
@@ -31,4 +31,21 @@ class MenuTest {
 
         assertEquals(2, menu.noOfMenuItems());
     }
+
+    @Test
+    void shouldAddAllMenuItemsToMenu() {
+        MenuItem menuItemOne = new ViewBooks();
+        MenuItem menuItemTwo = new CheckoutBook();
+        MenuItem menuItemThree = new ReturnBook();
+        MenuItem menuItemFour = new QuitApplication();
+        ArrayList<MenuItem> menuItems = new ArrayList<>();
+        menuItems.add(menuItemOne);
+        menuItems.add(menuItemTwo);
+        menuItems.add(menuItemThree);
+        menuItems.add(menuItemFour);
+
+        Menu menu = new Menu(menuItems);
+
+        assertEquals(4, menu.noOfMenuItems());
+    }
 }
