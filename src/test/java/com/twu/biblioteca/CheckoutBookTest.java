f51a03da6feaccf7e7ccbe25e511331bf5cd4eba
Aditi Aggarwal <aditi.aggarwal@thoughtworks.com>
2020-02-11 12:16:20 +0530

    [Aditi] Add. check for checkbook actionand desription

--- src/test/java/com/twu/biblioteca/CheckoutBookTest.java
+++ src/test/java/com/twu/biblioteca/CheckoutBookTest.java
@@ -1,7 +1,31 @@
 package com.twu.biblioteca;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 class CheckoutBookTest {
 
+    @Test
+    void shouldReturnCheckoutBookDescription() {
+        MenuItem checkoutBook = new QuitApplication("Quit Application");
+        String expectedOutput = "Quit Application";
+
+        String actualOutput = checkoutBook.description();
+
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    void shouldCallLibraryCheckout() {
+        MenuItem checkoutBook = new CheckoutBook("Checkout Book");
+        Library libraryMock = mock(Library.class);
+        Biblioteca bibliotecaMock = mock(Biblioteca.class);
+        when(bibliotecaMock.getBookNameEnteredByUser()).thenReturn("Harry Potter");
+
+        checkoutBook.action(libraryMock, bibliotecaMock);
+
+        verify(libraryMock, times(1)).checkoutBook("Harry Potter");
+    }
 }
